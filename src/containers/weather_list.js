d9efc453993f237f629997f52095baea6c081fec
Bryce Davidson <lifebryce@gmail.com>
2018-04-13 16:14:49 -0700

    added weather render properly

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
-
   renderWeather(cityData) {
+    const name = cityData.city.name;
     return (
-      <tr>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
       </tr>
     );
   };
